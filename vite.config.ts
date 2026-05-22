d6a8e5c21480d331743a93ac2cd5b43770c9228e
Lokesh Gandhi <lokeshgandhimodalavalasa111@gmail.com>
2026-05-22 18:17:16 +0530

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,9 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
+  vite: {
+    server: {
+      allowedHosts: ["city-tax-insights-main.onrender.com"],
+    },
+  },
 });
